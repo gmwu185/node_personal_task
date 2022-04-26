1a4feafc520d939e7c1fecf01d327e917d91693a
JimmyWu <gmwu185@gmail.com>
2022-04-27 05:19:15 +0800

    整理用不到的程式碼

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-// const routese = require('./routes')
 require('./connections'); // or require('./connections/index');
 
 const requestListener = async (req, res) => {
   require('./routes')(req, res);
-  // routese(req, res);
 };
 
 module.exports = requestListener;

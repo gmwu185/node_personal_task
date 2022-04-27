3937752a1c82a43c8344e14a6582870ef45ed8c0
JimmyWu <gmwu185@gmail.com>
2022-04-27 23:11:33 +0800

    修正 server.js 模組引入使用方式，以宣告變數提至前面行事區塊較集中

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-require('./connections'); // or require('./connections/index');
 const routes = require('./routes');
+require('./connections'); // or require('./connections/index');
 
 const requestListener = async (req, res) => {
   console.log(req.url, req.method); // 查看 client 使用路徑與 API 方法

7c7844482375db94821df35441c16f96354622b0
JimmyWu <gmwu185@gmail.com>
2022-05-05 23:39:25 +0800

    路由 GET /posts 寫法改用 express 簡寫

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/posts', function(req, res, next) {
-  postsControllors.getPosts({ req, res });
-});
+router.get('/posts', postsControllors.getPosts);
 router.post('/posts', function(req, res, next) {
   console.log('POST /posts');
   res.end();

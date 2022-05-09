39ce56cbe147b9035207069c0ac9e522c59216ba
JimmyWu <gmwu185@gmail.com>
2022-05-09 23:09:58 +0800

    swagger 文件初始與初步設定

---
+++ swagger.js
@@ -0,0 +1,14 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = { // 生成資料、格式、設定
+  info: {
+    title: 'Meta API',
+    description: '示範範例生成文件',
+  },
+  host: 'localhost:3000',
+  schemes: ['http', 'https'],
+};
+const outputFile = './swagger-output.json'; // 使用套件生成文件讀取用的 JSON 資料檔
+const endpointsFiles = ['./app.js']; // 專案系統的注入點，成為套件所讀取的檔案
+
+swaggerAutogen(outputFile, endpointsFiles, doc);

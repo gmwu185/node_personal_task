dd120cb89a496bdc2d1373946f25a0d601deace4
JimmyWu <gmwu185@gmail.com>
2022-05-29 00:45:51 +0800

    調整 swagger 文件內容

--- routes/posts.js
+++ routes/posts.js
@@ -10,21 +10,23 @@ router.get('/', (req, res, next) =>
     * #swagger.responses[200] = {
       description: '取得<strong>全部貼文</strong>',
       schema: {
-        "status": true,
-        "data": [
-          {
-            "_id": "123456789",
-            "name": "string",
-            "tags": [ "string-1", "string-2" ],
-            "type": "string",
-            "image": "",
-            "content": "string",
-            "likes": 0,
-            "comments": 0,
-            "__v": 0
-          },
-        ]
-      }
+        "_id": "62924730a3cca4c1b4a52226",
+        "userData": {
+          "_id": "62908a8ebf85a52ce75989a5",
+          "userName": "jimmyWu",
+          "email": "gg@mail.com",
+          "password": "123456",
+          "avatarUrl": "https://avatars.githubusercontent.com/u/42748910?v=4"
+        },
+        "content": "test",
+        "tags": [
+          "感情2"
+        ],
+        "type": "person",
+        "image": "http://",
+        "likes": 0,
+        "comments": 0
+      },
     }
   */
   PostsControllers.getPosts(req, res, next)
@@ -38,17 +40,30 @@ router.post('/', (req, res, next) =>
       required: true,
       description: `資料格式查看必填欄位，點按下方 Model 切換後，屬性欄位名稱的後方紅色的*`,
       schema: { 
-        $name: 'createdPost--test 加入 swagger.js 中',
-        $content: 'createdPost-test',
-        tags: ['string-1', 'string-2'],
-        type: 'string'
+        "$userData": "62908a8ebf85a52ce75989a5",
+        "$content": "test",
+        "$tags": ["感情2"],
+        "$type": "person",
+        "image": "http://"
       }
     },
-    * #swagger.responses[400] = {
-      description: '未帶上 name 的錯誤回應',
+    * #swagger.responses[200] = {
+      description: '建立貼正正確回應',
       schema: {
-        "status": "false",
-        "message": "Post validation failed: name: 貼文姓名未填寫"
+        "status": true,
+        "data": {
+          "userData": "62908a8ebf85a52ce75989a5",
+          "content": "test",
+          "tags": [
+            "感情2"
+          ],
+          "type": "person",
+          "image": "",
+          "likes": 0,
+          "comments": 0,
+          "_id": "62924e3af4568a486882612f",
+          "createAt": "2022-05-28T16:30:50.165Z"
+        }
       }
     }
    */
@@ -61,11 +76,8 @@ router.delete('/', (req, res, next) =>
   PostsControllers.delALLPosts(req, res, next)
 );
 router.delete('/:id', (req, res, next) =>
-  /** #swagger.tags = ['posts (貼文)']
-   * #swagger.description = '刪除單筆貼文'
-   * #swagger.security = [{
-      'apiKeyAuth': []
-    }],
+  /** #swagger.description = '刪除單筆貼文'
+   * #swagger.tags = ['posts (貼文)']
    * #swagger.parameters['id'] = {
       in: 'path',
       type: 'string',
@@ -88,13 +100,38 @@ router.patch('/:id', (req, res, next) =>
       required: true,
       description: "Body 資料格式",
       schema: { 
-        "name": "test-edit",
-        "content": "test-edit",
+        "$content": "test-edit",
         "tags": ["string-1", "string-2"],
-        "type": "string"
+        "type": "string",
+        "type": "string",
+        "image": "http://",
+        "likes": 0,
+        "comments": 0
+      }
+    },
+    * #swagger.responses[200] = {
+      description: '建立貼正正確回應',
+      schema: {
+        "status": true,
+        "data": {
+          "status": true,
+          "data": {
+            "_id": "62924fade5520c8a5493626f",
+            "userData": "62924fade5520c8a5493626f",
+            "content": "test-edit",
+            "tags": [
+              "string"
+            ],
+            "type": "string",
+            "image": "http://",
+            "likes": 0,
+            "comments": 0
+          }
+        }
       }
     }
    */
+    
   PostsControllers.upDatePost(req, res, next)
 );
 

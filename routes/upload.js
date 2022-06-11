edd52d1cbe8130d25321a49089fdc09667176675
JimmyWu <gmwu185@gmail.com>
2022-06-11 10:08:03 +0800

    1. 新增上傳圖片 swagger 文件說明   2. 修正登入、註冊文件格式錯誤 (不用登入)

--- routes/upload.js
+++ routes/upload.js
@@ -7,8 +7,37 @@ const router = express.Router();
 const { isAuth } = require('../handStates/auth');
 const upload = require('../service/image');
 
-router.post('/', isAuth, upload, (req, res, next) =>
-  UploadControllers.upLoadAvatarImg(req, res, next)
+router.post(
+  '/',
+  isAuth,
+  upload,
+  /** #swagger.summary = '上傳圖片'
+    * #swagger.tags = ['upload 上傳圖片']
+    * #swagger.description = '上傳圖片',
+    * #swagger.security = [{
+      'apiKeyAuth': []
+    }],
+    * #swagger.parameters['singleFile'] = {
+      in: 'formData',
+      type: 'file',
+      required: 'true',
+      description: `
+        <ul>
+          <li>圖片不得大於 1024 * 1024</li>
+          <li>僅限上傳 jpg、jpeg 與 png 格式。</li>
+          <li>圖片長寬不符合 1:1 尺寸</li>
+        </ul>
+      `,
+    }
+    * #swagger.responses[200] = {
+      description: '圖片上傳成功回應',
+      schema: {
+        "status": true,
+        "data": "https://i.imgur.com/xxxxx.jpg",
+      }
+    }
+   */
+  (req, res, next) => UploadControllers.upLoadAvatarImg(req, res, next)
 );
 
 module.exports = router;

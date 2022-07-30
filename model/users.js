0041aaa24126304956887ec8b720ebcae528fd0f
JimmyWu <gmwu185@gmail.com>
2022-07-31 01:12:24 +0800

    修正 followers 與 following user 資料庫關連方式

--- model/users.js
+++ model/users.js
@@ -52,16 +52,38 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-// 若有 find 則時觸發 following 虛擬掛載
+/** 若有 find 則時觸發 following 虛擬掛載
+ * 在 .populate() 所傳物件屬性 path，只能指定一個 document 做為關連對象
+ * 11. 多层级填充 (https://itbilu.com/nodejs/npm/HkAKMTECm.html)
+ */
 userSchema.pre(/^find/, function (next) {
+  /** 寫法一：
+   * 一張表能掛載進 2 個 document，
+   * populate 屬性使用只能用於一個 document (這裡指給 following)
+   * userData following 可關聯，帶 id 部份無法關聯
+   */
   this.populate({
     path: 'following',
     populate: {
       path: 'userData',
       model: 'user',
-      select: 'userName userId',
+      select: 'userName createdAt avatarUrl',
     },
-  });
+  }).populate('followers');
+
+  /** 寫法二：(初步寫法)
+   * 一張表只能載進 following
+   * userData 帶 id 部份成功關聯
+   */
+  // this.populate({
+  //   path: 'following', // deep-populate 深關聯 collection 一個 document
+  //   populate: {
+  //     path: 'userData',
+  //     model: 'user',
+  //     select: 'userName',
+  //   },
+  // });
+
   next();
 });
 
